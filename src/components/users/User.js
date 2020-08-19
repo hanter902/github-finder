7dce57144062525b27584df6a1c5cd1021803668
xuan <dangthanhxuan902@gmail.com>
2020-08-19 21:52:40 +0700

    useContext and useReducer

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,16 +1,19 @@
-import React, { Fragment, useEffect } from 'react';
+import React, { Fragment, useEffect, useContext } from 'react';
 import Spinner from '../layout/Spinner';
 import Repos from '../repos/Repos';
-import PropTypes from 'prop-types';
 import {Link} from 'react-router-dom';
+import GithubContext from '../../context/github/githubContext';
 
-const User =({user, loading, getUser, getUserRepos, repos, match}) =>{
+const User =({ match }) =>{
+
+    const githubContext = useContext(GithubContext);
+    const {getUser, loading, user, repos, getUserRepos} = githubContext;
 
     useEffect(()=> {
         getUser(match.params.login);
         getUserRepos(match.params.login);
         // eslint-disable-next-line
-    }, []);
+    }, []);    
 
     const {name, avatar_url, location, bio, blog, login, company, html_url, followers, following, public_repos, public_gists, hireable} = user;
 
@@ -64,12 +67,5 @@ const User =({user, loading, getUser, getUserRepos, repos, match}) =>{
     )
 }
 
-User.propTypes = {
-    loading: PropTypes.bool,
-    user: PropTypes.object.isRequired,
-    repos: PropTypes.array.isRequired,
-    getUser: PropTypes.func.isRequired,
-    getUserRepos: PropTypes.func.isRequired
-}
 
 export default User;
